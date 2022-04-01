86d0a7ea655ad1d10856c2c5bc73079eb89dc977
Andrew Brown <92134285+andrewj-brown@users.noreply.github.com>
2022-04-01 21:11:15 +1000

    Update in-the-pudding.py

--- 2022/in-the-pudding/in-the-pudding.py
+++ 2022/in-the-pudding/in-the-pudding.py
@@ -2,7 +2,7 @@
 It's taken us years of effort and hard work to reach the point of being able to write
 this question. We decided CodeJam would be the best place to announce this world-
 changing discovery, but we have several papers currently undergoing peer-review for
-publication in mathematical journals. We hope you enjoy our work:'''
+publication in mathematical journals. We hope you enjoy our work!'''
 
 print("N=1")
 
