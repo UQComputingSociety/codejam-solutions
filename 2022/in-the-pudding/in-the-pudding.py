dbc6f40bcc0a60bd0f5af9227126e1ed8604b962
Andrew Brown <92134285+andrewj-brown@users.noreply.github.com>
2022-04-01 21:10:48 +1000

    Create in-the-pudding.py

---
+++ 2022/in-the-pudding/in-the-pudding.py
@@ -0,0 +1,9 @@
+''' This proof is very long and complicated. We don't expect anyone to understand it.
+It's taken us years of effort and hard work to reach the point of being able to write
+this question. We decided CodeJam would be the best place to announce this world-
+changing discovery, but we have several papers currently undergoing peer-review for
+publication in mathematical journals. We hope you enjoy our work:'''
+
+print("N=1")
+
+# gottem
