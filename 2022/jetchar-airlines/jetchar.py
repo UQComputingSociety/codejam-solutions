f4a93b283c84e39f37081cde8826073c0d824a25
LimaoC <80520563+LimaoC@users.noreply.github.com>
2022-04-02 01:48:26 +1000

    Modify JetChar

--- 2022/jetchar-airlines/jetchar.py
+++ 2022/jetchar-airlines/jetchar.py
@@ -1,48 +1,23 @@
+def JetChar():
+    # greedy algorithm problem.
 
-def JetChar(items, numPeople):
-    d = float(7)
-    n = numPeople
-    s = 0
+    n = int(input())
 
-    items = items.split(" ")
-    #print(items)
-    # Convert items list to the number of characters in each
-    char_nums = []
-    for i in range(len(items)):
-        char_nums.append(len(items[i]))
+    # parse input and store in (start, end) format in a list
+    suitcases = []
+    for _ in range(n):
+        start, end = map(int, input().split())
+        suitcases.append((start, end))
 
-    items = char_nums
-    #print (items)
-    weighted = []
-    # Determine the average item value
-    for el in items:
-        f = el/d
-        s+= f
-        weighted.append(f)
+    # sort by ending time
+    suitcases.sort(key=lambda x: x[1])
 
-    avg_weight = s/n
-
-    els = []
+    current_suitcase_end = -1
+    out = 0
     for i in range(n):
-        els.append(0)
+        if suitcases[i][0] >= current_suitcase_end:  # suitcase can be stolen
+            # earliest ending suitcase that can be stolen
+            current_suitcase_end = suitcases[i][1]
+            out += 1
 
-    weighted.sort()
-    new_items = weighted
-
-    while (len(new_items) > 0):
-        els.sort()
-        els[0] += (new_items[len(new_items)-1])
-        del new_items[len(new_items)-1]
-
-    els.sort()
-    #print(els)
-    if (els[n-1] > 7):
-        return(-1)
-    else:
-        return (els[0])
-
-if __name__ == "__main__":
-    items = input("Enter items: ")
-    people = int(input("Enter number of people: "))
-
-    print(JetChar(items, people))
+    return out
